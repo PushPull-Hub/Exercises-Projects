de84a0be8a60f81964eb9ecf0806ebb86ff57eda
Dhn.coding <perro122@gmail.com>
2020-02-18 18:21:22 +0100

    Add Juventus project

---
+++ Projects/LucieChild1.js
@@ -0,0 +1,42 @@
+let answer = prompt(
+  " guessing number game  , choose a number x from n in your mind then type ok to start "
+);
+if (answer === "ok" && !(answer === null)) {
+  alert("ok multiply it by 2");
+  alert("ok now add to it 30");
+  alert("devide it by 2");
+  alert("add to it 3");
+  alert(" subtract from it the first number you choosed ");
+  alert("divide it by 2 now");
+  alert(" sum is  9 hahahahhaha ");
+}
+
+let answer2 = prompt("type yes , if you want to tryyy again ");
+
+if (answer2 === "yes" && !(answer === null)) {
+  alert("ok chooose an other number x from n");
+  alert("multiply it by 3");
+  alert("add 60");
+  alert("subtract 30");
+  alert("devide it by 3");
+  alert("subtract the number x");
+  alert("add 35");
+  alert("devide it by 9");
+
+  alert(" then you have 5 ");
+}
+
+let answer3 = prompt("you can still try again .... just type ok to confirm ");
+alert("ooooh hard to be satisfied hahahahaha");
+answer4 = prompt("ok we will try with big numbers now , type ok  ");
+
+if (answer4 === "ok" && !(answer === null)) {
+  alert(" ok choose a number x from n as always");
+  alert("ok now add to it 111");
+  alert("multiply it by 4 ");
+  alert("add 60 then devide it by 2");
+  alert("devide it by 2");
+  alert(" subtract from it the first number x ");
+  alert("devide it by 749");
+  alert("then you have 0.1682243");
+}
