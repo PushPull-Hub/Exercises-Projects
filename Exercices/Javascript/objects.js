221716d2ee10a66c2abe38700a334c3fc75fe5d5
Dhn.coding <perro122@gmail.com>
2020-02-18 09:29:11 +0100

    Objects Exercises

---
+++ Exercices/Javascript/objects.js
@@ -0,0 +1,88 @@
+// console.table()
+// Brainfuck
+//
+//
+//
+//
+//
+//
+//
+//
+
+// let user = {
+//   name: `Hamza`,
+//   age: `22`,
+//   city: `Turin`,
+//   "likes birds": true,
+//   isAdmin: true
+//     calss: `george`
+// };
+// delete user.calss;
+// user.name = "Pete";
+// console.log([user.calss]);
+//   let key = prompt(`what do you want to know ??`);
+//   alert(user[key])
+
+// let user = {
+//   name: `Hamza`,
+//   age: `22`,
+//   city: `Turin`,
+//   "likes birds": true,
+//   isAdmin: true
+// };
+
+// console.log("like birds" in user);
+// console.log("likes birds" in user);
+
+// let user = {
+//     name: `Hamza`,
+//     age: `22`,
+//     city: `Turin`,
+//     "likes birds": true,
+//     isAdmin: true,
+//     calss: `george`,
+//     money: undefined
+//   };
+// console.log(user.money);
+// console.log("money" in user);
+
+// let user = {
+//   name: `Hamza`,
+//   age: `22`,
+//   city: `Turin`,
+//   "likes birds": true,
+//   isAdmin: true,
+//   calss: `george`,
+//   money: undefined
+// };
+
+// for (let secondUser in user) {
+//   console.log(secondUser);
+// }
+// clone
+// let user = {
+//   name: `Hamza`,
+//   age: `22`,
+//   city: `Turin`,
+//   "likes birds": true,
+//   isAdmin: true,
+//   calss: `george`,
+//   money: undefined
+// };
+
+// let clone = {};
+
+// for (let copy in user) {
+//   clone[copy] = user[copy];
+// }
+// console.log(clone.age);
+
+let user = {
+  name: `Hamza`,
+  age: `22`,
+  city: `Turin`,
+  "likes birds": true,
+  isAdmin: true,
+  calss: `george`,
+  money: undefined
+};
