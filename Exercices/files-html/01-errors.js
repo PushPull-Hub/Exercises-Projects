3611631e70461148c817bee8e1b2132d04af7ad5
Dhn-Coding <60256254+Dhn-Coding@users.noreply.github.com>
2020-03-07 20:44:37 +0100

    After installing ESLINT

---
+++ Exercices/files-html/01-errors.js
@@ -0,0 +1,29 @@
+// try {
+//   console.log('before the Error');
+//   lalalal;
+//   console.log('after Errors');
+// } catch (err) {
+//   console.log('the Error is  ', err.stack);
+// }
+
+// console.log("heee it's working ");
+
+// let user = {
+//   name: 'Ahmed',
+//   age: 31,
+//   mass: 75,
+// };
+// console.log(user.name);
+
+// let admin = {
+//   name: 'Poco7',
+//   age: 18,
+//   pass: 'Password123',
+//   computer: 'Lenovo',
+// };
+
+// console.log(admin.age);
+
+// FETCH API
+
+fetch('https://swapi.code/api/people/').then;
