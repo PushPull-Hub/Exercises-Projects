b17e39718f70f2230d0ed3ad892f3ed59154f199
Dhn-Coding <60256254+Dhn-Coding@users.noreply.github.com>
2020-03-12 17:42:03 +0100

    Practicing some JavaScript

---
+++ Projects/new/script.js
@@ -0,0 +1,8 @@
+// HI
+
+function showName() {
+  let name = document.getElementById('input').value;
+  return (document.getElementById(
+    'paragraph',
+  ).innerHTML = `Hello <span class="item animated fadeInDown">${name}</span>`);
+}
