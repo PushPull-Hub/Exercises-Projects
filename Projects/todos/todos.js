3f0ca061cff479ee9318882d040a7ac71cb64411
Dhn-Coding <60256254+Dhn-Coding@users.noreply.github.com>
2020-03-19 14:20:56 +0100

    first commitment

--- Projects/todos/todos.js
+++ Projects/todos/todos.js
@@ -1,23 +1,72 @@
-let todos = [
-  { id: 1, text: `Learn JavaScript`, completed: true },
-  { id: 2, text: `Seek For a Job`, completed: false },
-  { id: 3, text: `Forgot Everything`, completed: true },
-];
+const clear = document.querySelector('.clear');
+const dateElement = document.getElementById('date');
+const list = document.getElementById('list');
+const input = document.getElementById('input');
 
-function render() {
-  document.querySelector(`ul`).innerHTML = todos
-    .map(todo => ` <li> ${todo.text}</li> `)
-    .join(`\n`);
+// .insertAdjacentElement to add the todo to the list
+
+const CHECK = 'fa-check-circle'; // Green
+const UNCHECK = 'fa-circle-thin'; // White
+const LINE_TROUGH = 'lineTrought';
+
+function addTodo(todo, id, done, trash) {
+  if (trash) {
+    return trash;
+  }
+  const DONE = done ? CHECK : UNCHECK;
+  const LINE = done ? LINE_TROUGH : '';
+  let parser = new DOMParser();
+  const text = parser.parseFromString(
+    `<li class="item"><i class="fa ${DONE} " job="complete" id ="${id}"></i><p class=  ${LINE} > ${todo} </p><i class="fa fa-trash-o delete" job="delete" id="${id}"></i></li>`,
+    'text/html',
+  );
+  const position = 'beforeend';
+  list.insertAdjacentElement(position, text);
 }
-render();
 
-document.querySelector('form').onsubmit = function(event) {
-  event.preventDefault();
+// when User press Enter Key _(key code 13)
+// get the input value return it to todo
+let List = [];
+let id = 0;
+input.addEventListener('keyup', function(event) {
+  if (event.keyCode == 13) {
+    const todo = input.value;
+    if (todo) {
+      addTodo(todo, id, false, false);
+      List.push({
+        name: todo,
+        id: id,
+        done: false,
+        trash: false,
+      });
+    }
+    input.value = '';
+    id++;
+  }
+});
 
-  let todo = { id: 4, text: 'Eat your vegetables' };
-  todos.push(todo);
-  render();
-};
+function completeTodo(element) {
+  element.classList.toggle(CHECK);
+  element.classList.toggle(UNCHECK);
+  element.parentNode.querySelector('.text').classList.toggle(LINE_TROUGH);
+  List[element.id].done = List[element.id].done ? false : true;
+}
 
-// function generateId (){
-// let newId= todo[ todos.lenght-1 ].id
+function removeTodo(element) {
+  let listItem = element.parentNode;
+  element.parentNode.parentNode.removeChild(listItem);
+  list[element.id].trash = true;
+}
+
+list.addEventListener('click', function(event) {
+  element = event.target;
+  const elementJob = event.target.attributes.job.value;
+  if (elementJob == 'complete') {
+    completeTodo(element);
+  }
+  if (elementJob == 'delete') {
+    removeTodo(element);
+  }
+});
+// Store on Browser
+localStorage.setItem('todo', JSON.stringify(List));
